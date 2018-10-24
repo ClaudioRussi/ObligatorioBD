10b03afcb780f15bd67c2498fb57c86861973b32
Agustín <Agustn@DESKTOP-8P3NSKA>
2018-10-24 14:40:33 -0300

    tercer commit

---
+++ src/obligatoriobd/Insumo.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package obligatoriobd;
+
+/**
+ *
+ * @author Agustín
+ */
+public class Insumo {
+    
+    public static int id = 0;
+    private int IDInsumo;
+    private String nombre;
+    private String descripcion;
+    
+    public Insumo(String nombre, String descripcion) {
+        IDInsumo = Insumo.id++;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
+    public static int getId() {
+        return id;
+    }
+
+    public static void setId(int id) {
+        Insumo.id = id;
+    }
+
+    public int getIDInsumo() {
+        return IDInsumo;
+    }
+
+    public void setIDInsumo(int IDInsumo) {
+        this.IDInsumo = IDInsumo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+}
