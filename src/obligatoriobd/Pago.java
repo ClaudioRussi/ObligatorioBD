12afb02015eb81892bbbe1c617806dd50ea6e71d
Claudio Russi <claudiomrussi@hotmail.com>
2018-11-11 22:36:10 -0300

    Aceptar pagos agregado

--- src/obligatoriobd/Pago.java
+++ src/obligatoriobd/Pago.java
@@ -5,6 +5,9 @@
  */
 package obligatoriobd;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -16,16 +19,38 @@ import java.util.Date;
 public class Pago {
     int idPago;
     int precio;
-    Date fecha;
+    Calendar fecha;
     int idUsuario;
     int idReunion;
+    boolean aceptado = false;
+    static private int ID = 0;
+    static boolean errorAlGuardar = false;
     
-    public Pago(int idPago, int precio, Date fecha, int idUsuario, int idReunion){
+    public Pago(int precio, Calendar fecha, int idUsuario, int idReunion, boolean aceptado){
+        ID++;
+        this.idPago = ID;
+        this.fecha = fecha;
+        this.precio = precio;
+        this.idUsuario = idUsuario;
+        this.idReunion = idReunion;
+        this.aceptado = aceptado;
+    }
+    
+    public Pago(int idPago, int precio, Calendar fecha, int idUsuario, int idReunion, boolean aceptado){
         this.idPago = idPago;
         this.fecha = fecha;
         this.precio = precio;
         this.idUsuario = idUsuario;
         this.idReunion = idReunion;
+        this.aceptado = aceptado;
+    }
+
+    public boolean isAceptado() {
+        return aceptado;
+    }
+
+    public void setAceptado(boolean aceptado) {
+        this.aceptado = aceptado;
     }
 
     public int getIdPago() {
@@ -36,7 +61,7 @@ public class Pago {
         return precio;
     }
 
-    public Date getFecha() {
+    public Calendar getFecha() {
         return fecha;
     }
 
@@ -56,7 +81,7 @@ public class Pago {
         this.precio = precio;
     }
 
-    public void setFecha(Date fecha) {
+    public void setFecha(Calendar fecha) {
         this.fecha = fecha;
     }
 
@@ -68,5 +93,85 @@ public class Pago {
         this.idReunion = idReunion;
     }
     
+    public void Save(){
+        try{
+            Class.forName("org.postgresql.Driver");
+            Connection conexion = DriverManager.getConnection(ObligatorioBD.url, ObligatorioBD.usuario, ObligatorioBD.contrasenia);
+            java.sql.Statement st = conexion.createStatement();
+            String insertion = "INSERT INTO pago VALUES ("+this.idPago+" ,"+this.precio+", '"
+                    +Herramientas.ConvertirCalendarAString(fecha)+"', "+ this.idUsuario +", "
+                    +this.idReunion+ ", " + this.aceptado +");";
+            st.executeUpdate(insertion);
+            st.close();
+            conexion.close();
+            errorAlGuardar = false;
+        }catch (SQLException e){
+            System.out.println("ERROR DE CONEXION " + e.getMessage());
+            errorAlGuardar = true;
+        }
+        catch(ClassNotFoundException e){
+            System.out.println("ERROR AL CARGAR LA CLASE "+ e.getMessage());
+            errorAlGuardar = true;
+        }
+    }
+    
+    static void buscarPagosPorReunion(int idReunion, ArrayList<Pago> pagos){
+        Compra compra = null;
+        try{
+            Class.forName("org.postgresql.Driver");
+            Connection conexion = DriverManager.getConnection(ObligatorioBD.url, ObligatorioBD.usuario, ObligatorioBD.contrasenia);
+            java.sql.Statement st = conexion.createStatement();
+            
+            String sql = "SELECT * FROM pago WHERE id_reunion = " + idReunion;
+            ResultSet result = st.executeQuery(sql);
+            while(result.next()){
+                Calendar fecha = Calendar.getInstance();
+                fecha.setTimeInMillis(result.getTimestamp("fecha").getTime());
+                pagos.add(new Pago(result.getInt("idPago"),result.getInt("precio"),fecha,result.getInt("idUsuario"),result.getInt("idReunion"),result.getBoolean("aceptado")));
+            }
+            result.close();
+            st.close();
+            conexion.close();
+        }catch (Exception e){
+            System.out.println("ERROR DE CONEXION" + e.getMessage());
+            
+        }
+    }
+    
+    void Update(){
+        try{
+            Class.forName("org.postgresql.Driver");
+            Connection conexion =  DriverManager.getConnection(ObligatorioBD.url, ObligatorioBD.usuario, ObligatorioBD.contrasenia);
+            java.sql.Statement st = conexion.createStatement();
+            String insertion = "UPDATE pago SET aceptado = "+this.idUsuario+ this.aceptado +" WHERE id_pago ="+ this.idPago;
+            st.executeUpdate(insertion);
+            st.close();
+            conexion.close();
+        }catch (SQLException e){
+            System.out.println("ERROR DE CONEXION " + e.getMessage());
+            
+        }
+        catch(ClassNotFoundException e){
+            System.out.println("ERROR AL CARGAR LA CLASE "+ e.getMessage());
+        }
+    }
+    
+    void Delete(){
+        try{
+            Class.forName("org.postgresql.Driver");
+            Connection conexion = DriverManager.getConnection(ObligatorioBD.url, ObligatorioBD.usuario, ObligatorioBD.contrasenia);
+            java.sql.Statement st = conexion.createStatement();
+            String Update = "DELETE FROM pago WHERE id_pago ="+this.idPago;
+            st.executeUpdate(Update);
+            st.close();
+            conexion.close();
+        }catch (SQLException e){
+            System.out.println("ERROR DE CONEXION " + e.getMessage());
+            
+        }
+        catch(ClassNotFoundException e){
+            System.out.println("ERROR AL CARGAR LA CLASE "+ e.getMessage());
+        }
+    }
     
 }
